04e28ae41e2637a89f3e60e536d883bf0b6f6caf
Ayodele obasemola <obasemolaayodele@gmail.com>
2020-06-23 22:16:33 +0300

    created cde to show and hide encrypted message and unencrypted message

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
 
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+
   const input = document.querySelector('#message-input');
   const encrypted = btoa(input.value);
 
-  document.querySelector(
-    '#link-input'
-    ).value = `${window.location}#${encrypted}`;
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
 });
