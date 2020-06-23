1c8f8a60c2a33da00fea8279c25052a951ceb709
Ayodele obasemola <obasemolaayodele@gmail.com>
2020-06-23 21:51:29 +0300

    created code to receive input value and encrypt it

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
+
+  document.querySelector(
+    '#link-input'
+    ).value = `${window.location}#${encrypted}`;
+});
