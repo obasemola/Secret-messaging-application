f021136d6f46045e0381f1d48a8ae2d82ef72bd1
Ayodele obasemola <obasemolaayodele@gmail.com>
2020-06-23 22:41:54 +0300

    created code to decrypt the encoded message and take user back to first page

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if(message) {
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+
+  document.querySelector('h1').textContent = message;
+}
+
+
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
 
